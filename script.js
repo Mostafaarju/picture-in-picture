f52ef002129ec317b9681633051ce2788c17962b
Raju Talukder <63764709+Mostafaarju@users.noreply.github.com>
2020-12-30 12:00:23 +0100

    PictureInPicture

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('Hello World');
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+button.addEventListener('click', async () => {
+    // Disable button
+    button.disabled = true;
+    // Start Picture in Picture
+    await videoElement.requestPictureInPicture();
+    // Reset Button
+    button.disabled = false;
+});
+
+selectMediaStream();
